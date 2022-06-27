73e301053f31313c20932d8e8a1f2296f580c87a
bsampp <90393235+bsampp@users.noreply.github.com>
2022-06-27 14:49:29 -0300

    Add files via upload

---
+++ POO1/ExercicioPessoa/src/TestePessoa.java
@@ -0,0 +1,10 @@
+
+public class TestePessoa {
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.nome = "Bruno";
+		pessoa1.telefone = "42998002561";
+		pessoa1.exibeDados();
+	}
+
+}
